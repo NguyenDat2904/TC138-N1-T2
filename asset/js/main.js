c2b560f4a8b2f37b904721ee469e796c06f5bc78
Dat Nguyen <dathhcc2@gmail.com>
2023-05-23 11:00:17 +0700

    fix Hung,Vu

--- asset/js/main.js
+++ asset/js/main.js
@@ -80,25 +80,25 @@ function eventUser() {
   let interDuce = document.querySelector(".interducePage");
   interDuce.addEventListener("click", () => {
     homePage.classList.add("active");
-    interPage.classList.remove("active");
-    signupPage.classList.add("active");
-    signinPage.classList.add("active");
+    interPage.classList.add("active");
+    signupPage.classList.remove("active");
+    signinPage.classList.remove("active");
   });
 
   // Home Page
   let home_page = document.querySelector(".homePage");
   home_page.addEventListener("click", () => {
     homePage.classList.remove("active");
-    interPage.classList.add("active");
-    signupPage.classList.add("active");
-    signinPage.classList.add("active");
+    interPage.classList.remove("active");
+    signupPage.classList.remove("active");
+    signinPage.classList.remove("active");
   });
 
   // Signin
   let signIn = document.querySelector(".sign_in");
   signIn.addEventListener("click", () => {
     signinPage.classList.remove("active");
-    interPage.classList.add("active");
+    interPage.classList.remove("active");
     homePage.classList.add("active");
     signupPage.classList.add("active");
   });
@@ -107,7 +107,7 @@ function eventUser() {
   signUn.addEventListener("click", () => {
     signupPage.classList.remove("active");
     signinPage.classList.add("active");
-    interPage.classList.add("active");
+    interPage.classList.remove("active");
     homePage.classList.add("active");
   });
 }
@@ -241,32 +241,26 @@ function start() {
 }
 start();
 // Hung
-const loginForm=document.getElementById("login")
-const errorEl=document.querySelector(".errorMsg")
+const loginForm = document.getElementById("login");
+const errorEl = document.querySelector(".errorMsg");
 
-loginForm.addEventListener("submit", function(event){
-    event.preventDefault();
-    const emailEl=loginForm.email;
-    const emaill=emailEl.value;
-    const relexEmail=/^\w{4,}@\w+\.\w+$/;
-    if(emaill===" ")
-    {
-        errorEl.innerHTML="Vui lòng không để chống thông tin";
-        emailEl.classList.add("errorborder");
-    }
-    else if(emaill.length<8)
-    {
-        errorEl.innerHTML="Email phải có ít nhất 8 ký tự.Ví dụ : infor@gmail.com";
-        emailEl.classList.add("errorborder");
-    }
-    else if(relexEmail.test(emaill)===false)
-    {
-        errorEl.innerHTML="Vui lòng điền đúng định dạng Email. Ví dụ: infor@gmail.com";
-        emailEl.classList.add("errorborder");
-    }
-    else
-    {
-        errorEl.innerHTML="";
-        emailEl.classList.remove("errorborder");
-    }
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const emailEl = loginForm.email;
+  const emaill = emailEl.value;
+  const relexEmail = /^\w{4,}@\w+\.\w+$/;
+  if (emaill === " ") {
+    errorEl.innerHTML = "Vui lòng không để chống thông tin";
+    emailEl.classList.add("errorborder");
+  } else if (emaill.length < 8) {
+    errorEl.innerHTML = "Email phải có ít nhất 8 ký tự.Ví dụ : infor@gmail.com";
+    emailEl.classList.add("errorborder");
+  } else if (relexEmail.test(emaill) === false) {
+    errorEl.innerHTML =
+      "Vui lòng điền đúng định dạng Email. Ví dụ: infor@gmail.com";
+    emailEl.classList.add("errorborder");
+  } else {
+    errorEl.innerHTML = "";
+    emailEl.classList.remove("errorborder");
+  }
 });
